85a3865675d5039c83b14ecba0428658a99a088a
qimenglong <qyt1306@163.com>
2018-10-11 17:15:42 +0800

    4.log

--- RunAllTestcase/runall.py
+++ RunAllTestcase/runall.py
@@ -18,8 +18,9 @@ def RunAllInterface():
     runner = HTMLTestRunner.HTMLTestRunner(stream=fp,title='CNPC登录接口测试报告',description='用例执行情况')
     #将所有用例加入测试套
     base_dir = os.path.dirname(os.path.dirname(__file__))
-    test_dir = os.path.join(base_dir,'testcase/login')
-    test_suit = unittest.defaultTestLoader.discover(test_dir,pattern='*.py')
+    test_dir = os.path.join(base_dir,'testcase')
+    test_dir02 = os.path.join(test_dir,'login')
+    test_suit = unittest.defaultTestLoader.discover(test_dir02,pattern='*.py')
     runner.run(test_suit)
     fp.close()
 
